4ce2d30e4e1778ad08d05b7c55ecc2de94b2f275
msk <msky0912@gmail.com>
2023-10-10 23:03:52 +0900

    Thread Pool을 적용해서 서비스하도록 처리하기

--- src/main/java/com/example/waspractice2/CustomWebApplicationServer.java
+++ src/main/java/com/example/waspractice2/CustomWebApplicationServer.java
@@ -9,12 +9,18 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // tomcat 만들어보기
 // Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+// Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+// Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -36,7 +42,12 @@ public class CustomWebApplicationServer {
                  * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
                  * 제한없이 요청마다 스레드를 생성하면 서버에 부하가 커진다. -> pool 로 해결해야함.
                  */
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                //new Thread(new ClientRequestHandler(clientSocket)).start();
+
+                /**
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                 */
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
